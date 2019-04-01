48adc1d769aed6885df0dd7d9339e5f5aa0a4650
vijaybarve <vjaybarve@rediffmail.com>
2019-03-31 22:37:51 -0400

    V 0.0.18 fxn list_wiki_syn added
    fxn list_wiki_syn added which replaces GetWikiSyn fxn

--- R/list_wiki_syn.R
+++ R/list_wiki_syn.R
@@ -1,11 +1,9 @@
 #' Get Wikipedia Synonyms for list of names
 #'
-#' This function is depreciated. Please use \link{list_wiki_syn}
-#'
 #' Fetch Synonyms from Wikipedia and clean them for use
 #'
 #' @param namelist list of scientific names
-#' @family Discontinued functions
+#' @family Wiki functions
 #' @return a data frame containing names, synonyms and Canonical synonyms matched
 #'    with GBIF backbone taxonomy  \itemize{ \item{Name}  {: Scientific name}
 #'    \item{OrigSyn}  {: Original synonym returned by Wikipedia}
@@ -13,12 +11,11 @@
 #' @importFrom wikitaxa wt_wikipedia
 #' @importFrom taxize gbif_parse
 #' @examples
-#' #GetWikiSyn("Abrothrix illutea")
-#' #GetWikiSyn(c("Abditomys latidens", "Abeomelomys sevia", "Abrocoma schistacea"))
+#' list_wiki_syn("Abrothrix illutea")
+#' #list_wiki_syn(c("Abditomys latidens", "Abeomelomys sevia", "Abrocoma schistacea"))
 #'
 #' @export
-GetWikiSyn <- function(namelist){
-  .Deprecated("ist_wiki_syn")
+list_wiki_syn <- function(namelist){
   res <- NULL
   for(i in 1:length(namelist)){
     accname <- namelist[i]
