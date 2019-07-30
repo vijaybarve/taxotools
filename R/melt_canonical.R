5e07235966c6fc678d9c6453649f1f71c80a9ee7
vijaybarve <vjaybarve@rediffmail.com>
2019-07-30 13:16:23 -0400

    V 0.0.37 Fix #24 in fxn melt_canonical
    Fix for issue #24 in fxn melt_canonical

--- R/melt_canonical.R
+++ R/melt_canonical.R
@@ -16,25 +16,26 @@
 #' }
 #' @export
 melt_canonical <- function(dat,canonical="",genus="",species="",subspecies=""){
-  reglst <- c("f.","sp.", "var.","ab.","spp.","cf.")
   newdat <- as.data.frame(dat)
   if(genus==""){
     return(NULL)
   } else {
     newdat <- rename_column(newdat,genus,"genus")
+    newdat$genus <- NA
   }
   if(species==""){
     return(NULL)
   } else {
     newdat <- rename_column(newdat,species,"species")
+    newdat$species <- NA
   }
   if(subspecies!=""){
     newdat <- rename_column(newdat,subspecies,"subspecies")
+    newdat$subspecies <- NA
   }
   if(canonical!=""){
     newdat <- rename_column(newdat,canonical,"canonical")
   }
-  newdat$canonical <- trimws(newdat$canonical)
   for(i in 1:dim(newdat)[1]){
     if(!is.empty(newdat$canonical[i])){
       tl <- guess_taxo_level(newdat$canonical[i])
@@ -44,34 +45,6 @@ melt_canonical <- function(dat,canonical="",genus="",species="",subspecies=""){
       }
       if(tl=="Subspecies" & subspecies!=""){
         newdat$subspecies[i] <- strsplit(newdat$canonical[i]," ")[[1]][3]
-      }
-      if(tl=="Unknown"){
-        nwords <- length(strsplit(newdat$canonical[i]," ")[[1]])
-        ct <- 2
-        sp <- strsplit(newdat$canonical[i]," ")[[1]][ct]
-        # If the names is in (), it will be a subgenus and not species
-        if(substr(sp,1,1)=="("){
-          ct <- ct+1
-          sp <- strsplit(newdat$canonical[i]," ")[[1]][ct]
-        }
-        ct <- ct+1
-        spp <- strsplit(newdat$canonical[i]," ")[[1]][ct]
-        if(spp=="spp."){
-          ct <- ct+1
-          if(nwords>=ct){
-            spp <- strsplit(newdat$canonical[i]," ")[[1]][ct]
-          }
-        }
-        # Subspecies name should not start with Capital character. That will be author
-        if(isupper(substr(spp,1,1))){
-          spp <- ""
-        }
-        # We need to remove certain strings like sp. f. etc.
-        if(spp %in% reglst){
-          spp <- ""
-        }
-        newdat$species[i] <- sp
-        newdat$subspecies[i] <- spp
       }
     }
   }
