2087d42b17cecb554f3f6fdd2c4826353b6ac030
vijaybarve <vjaybarve@rediffmail.com>
2020-09-14 17:59:25 -0400

    V 0.0.64 synonymize_subspecies has verbose param #52
    verbose param in fxn synonymize_subspecies

--- R/synonymize_subspecies.R
+++ R/synonymize_subspecies.R
@@ -1,6 +1,7 @@
 #' @title Convert all subspecies into synonyms of the species
 #' @description used in generating master lists
 #' @param master List of names with a field named canonical
+#' @param verbose display process messages, Default: FALSE
 #' @return Same list of names with id and accid fields added (or data updated
 #' the fields exists) with all subspecies linked to the species names as
 #' synonyms
@@ -11,7 +12,8 @@
 #' }
 #' @rdname synonymize_subspecies
 #' @export
-synonymize_subspecies <- function(master){
+synonymize_subspecies <- function(master,
+                                  verbose=FALSE){
   names(master) <- tolower(names(master))
   if("id" %!in% names(master)){
     master$id <- seq.int(nrow(master))
@@ -24,7 +26,7 @@ synonymize_subspecies <- function(master){
        & master$accid[i]==0){
       spname <- paste(unlist(strsplit(master$canonical[i],
                                       split = "\\s+"))[1:2],collapse=" ")
-      print(paste(i,master$canonical[i]))
+      if(verbose){print(paste(i,master$canonical[i]))}
       master$accid[i] <- master$id[which(master$canonical==spname)]
     }
   }
