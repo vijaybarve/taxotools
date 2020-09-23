f08e695f46e41005d29cd1c3ad62bba30c1c17c2
vijaybarve <vjaybarve@rediffmail.com>
2020-09-22 23:23:36 -0400

    V 0.0.66 fxn DwC2taxo handles GBIF data #55
    DwC2taxo handles GBIF data

--- R/DwC2taxo.R
+++ R/DwC2taxo.R
@@ -6,6 +6,7 @@
 #' all the required fields. Just needs to be converted and quality checked in terms
 #'  of missing linkages
 #' @importFrom plyr rename
+#' @importFrom stringr word
 #' @examples
 #' \dontrun{
 #' if(interactive()){
@@ -15,20 +16,43 @@
 #' @rdname DwC2taxo
 #' @export
 DwC2taxo <- function(namelist){
-  namelist <- namelist[which(namelist$taxonRank == "species" |
-                               namelist$taxonRank == "subspecies"),]
-  namelist <- rename(namelist,
-                     replace = c("taxonID" = "id",
-                                 "acceptedNameUsageID" = "accid",
-                                 "specificEpithet" = "species",
-                                 "infraspecificEpithet" = "subspecies",
-                                 "taxonRank" = "taxonlevel"))
-  # melt_authyear
+  if("taxonRank" %in% names((namelist))){
+    namelist <- namelist[which(toupper(namelist$taxonRank) == "SPECIES" |
+                                 toupper(namelist$taxonRank) == "SUBSPECIES"),]
+  } else {
+    warning("taxonRank not found.")
+    return(NULL)
+  }
+  if("taxonomicStatus" %in% names((namelist))){
+    namelist <- namelist[which(toupper(namelist$taxonomicStatus) == "ACCEPTED" |
+                                 toupper(namelist$taxonomicStatus) == "SYNONYM"),]
+  }
+  if("taxonID" %in% names((namelist))){
+    namelist <- rename(namelist,
+                       replace = c("taxonID" = "id",
+                                   "acceptedNameUsageID" = "accid",
+                                   "specificEpithet" = "species",
+                                   "infraspecificEpithet" = "subspecies",
+                                   "taxonRank" = "taxonlevel"))
+  }
+  if("taxonKey" %in% names((namelist))){
+    namelist <- rename(namelist,
+                       replace = c("taxonKey" = "id",
+                                   "acceptedTaxonKey" = "accid",
+                                   "taxonRank" = "taxonlevel"))
+    namelist$species <- word(namelist$species,2)
+    namelist$accid[which(namelist$accid==namelist$id)] <- 0
+    if("subspecies" %!in% names(namelist)){
+      namelist <- melt_scientificname(namelist,
+                                      sciname = "scientificName",
+                                      genus = "genus_",
+                                      species = "species_",
+                                      subspecies = "subspecies")
+    }
+  }
   namelist[which(namelist$accid %in% namelist$id),]
   namelist <- cast_canonical(namelist,"canonical","genus","species","subspecies")
   namelist <- namelist[,c("id", "order", "family", "genus", "species",
                           "subspecies", "taxonlevel", "accid", "canonical")]
   return(namelist)
 }
-
-
