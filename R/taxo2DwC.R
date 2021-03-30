09d2d310f9e02f556099819ecb5b181fc8b4ee53
Vijaybarve <vbarve@flmnh.ufl.edu>
2021-03-29 23:30:15 -0400

    V 0.0.101 param verbose addded to taxo2DwC and field validation #94

--- R/taxo2DwC.R
+++ R/taxo2DwC.R
@@ -1,6 +1,7 @@
 #' @title Taxolist to Darwin Core (DwC)
 #' @description Converts a taxolist to Darwin Core format
 #' @param taxolist taxolist
+#' @param verbose verbose output, Default: TRUE
 #' @return returns a taxonomic list in DwC format
 #' @details Converts a taxolist to Darwin Core format
 #' @family List functions
@@ -26,16 +27,51 @@
 #' @rdname taxo2DwC
 #' @export
 #' 
-taxo2DwC <- function(taxolist){
+taxo2DwC <- function(taxolist,
+                     verbose=TRUE){
   if(is.null(taxolist)){
     return(NULL)
   }
   if(nrow(taxolist)<1){
     return(NULL)
   }
+  if("canonical" %!in% names(taxolist)){
+    if("genus" %!in% names(taxolist) | "species" %!in% names(taxolist)){
+      stop("Column names are not in standard format or missing some columns")
+    } else {
+      if(verbose){cat(paste("\nUpdating canonical names...\n"))}
+      taxolist <- cast_canonical(taxolist,
+                                 canonical = "canonical",
+                                 genus = "genus",
+                                 species = "species",
+                                 subspecies = "subspecies",
+                                 verbose=verbose)
+    }
+  }
+  if("genus" %!in% names(taxolist)){
+    if("canonical" %!in% names(taxolist)){
+      stop("Column names are not in standard format or missing some columns")
+    } else {
+      if(verbose){cat(paste("\nUpdating genus, species, subspecies...\n"))}
+      taxolist <- melt_canonical(taxolist,
+                                 canonical = "canonical",
+                                 genus = "genus",
+                                 species = "species",
+                                 subspecies = "subspecies",
+                                 verbose=verbose)
+    }
+  }
   if("taxonlevel" %!in% names(taxolist)){
     taxolist$taxonlevel <- as.character(lapply(taxolist$canonical, 
                                                guess_taxo_rank))
+  }
+  if("id" %!in% names(taxolist)){
+    taxolist$id <- seq(1:nrow(taxolist))
+    warning("Column 'id' was missing. Assigning ids to each name")
+  }
+  if("accid" %!in% names(taxolist)){
+    taxolist$accid <- 0L
+    warning("Column 'accid' was missing. Assuming all names are accepted names")
   }
   taxolist <- rename_column(taxolist,"id","taxonKey")
   taxolist <- rename_column(taxolist,"canonical","scientificName")
@@ -56,7 +92,7 @@ taxo2DwC <- function(taxolist){
     taxo_syn$acceptedScientificName <- taxo_ac$scientificName[match(taxo_syn$acceptedTaxonKey,taxo_ac$taxonKey)]
     taxo_ac <- rbind(taxo_ac,taxo_syn)
   }
-  taxo_ac <- taxo_ac[,!names(taxo_ac) %in%c("accid")]
+  taxo_ac <- taxo_ac[,!names(taxo_ac) %in% c("accid")]
   return(taxo_ac)
 }
 
